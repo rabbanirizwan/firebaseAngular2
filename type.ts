a85cb6091d04cb6b741e83afdcc2bb2010e11997
rabbanirizwan <33807776+rabbanirizwan@users.noreply.github.com>
2019-10-23 19:20:59 +0500

    Add files via upload
    type script basic

---
+++ type.ts
@@ -0,0 +1,22 @@
+interface Person{
+  fname:string;
+ lname:string;
+ [key:string]:any;
+}
+
+const Person1 : Person={
+    fname:"RIzwan",
+    lname:"Rabbani"
+}
+const Person3:Person={
+    fname:"Rizwan",
+    lname:"jjsj",
+    k:"sdsd"
+
+}
+
+
+const array:number[]=[]
+
+array.push(1);
+array.push("1")
